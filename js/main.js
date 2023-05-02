f4490c3d14201946229e964ce00a90100bbe8e07
alex4rom <alex4rom721@gmail.com>
2023-05-02 10:15:21 -0400

    update

--- js/main.js
+++
@@ -1,6 +0,0 @@
-console.log("Hello! Welcome to RomeroDesign.net, my protfolio site!");
-
-function menuToggle() { 
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {x.className += ' responsive'; } else { x.className = 'navtoggle'; }
-}
